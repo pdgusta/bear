4593c4445983a36857fe4537489900b06525d593
Gustavo Pott Delfino <gustavopottpott@gmail.com>
2022-11-04 22:34:13 -0300

    adds random functionality to 'bear'

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+let word = "Bear"
+function stringToArray(str) {
+    let arr = []
+    for (let i = 0; i < str.length; i++){
+        arr.push(str[i])
+    }
+    return arr
+}
+function variar(str){
+    let arr = stringToArray(str)
+    for (let i = arr.length -1; i>0; i--){
+        const j= Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+
+    return arr
+}
+
 class Bear extends React.Component {
     render() {
         return (
             <div id="corpo" className="center">
                 <div>
-                    <h1>Bear!</h1>
-
+                    <h1>{variar(word)}</h1>
                 </div>
             </div>
         );

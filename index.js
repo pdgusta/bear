eb16a47c90aa24d5aac5e27c77183ea459c2b8a6
Gustavo Pott Delfino <47679656+pdgusta@users.noreply.github.com>
2025-06-12 16:49:58 -0300

    fix: add space after j assignment

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function stringToArray(str) {
 function variar(str){
     let arr = stringToArray(str)
     for (let i = arr.length -1; i>0; i--){
-        const j= Math.floor(Math.random() * (i + 1));
+        const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]]
     }
 
